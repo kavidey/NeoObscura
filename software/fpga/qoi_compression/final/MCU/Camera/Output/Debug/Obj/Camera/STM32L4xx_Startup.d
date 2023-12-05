5bd15dcd726cbff321bcaf4281328e77b6cfb4b8
kavidey <kaviasher@gmail.com>
2023-12-05 14:03:22 -0800

    Clean up FPGA code

--- software/fpga/qoi_compression/final/MCU/Camera/Output/Debug/Obj/Camera/STM32L4xx_Startup.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/Camera/STM32L4xx_Startup.o: \
- /Users/neilchulani/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/Camera/STM32L4xx/Source/STM32L4xx_Startup.s
--- software/fpga/qoi_compression/qoi_ram/Camera/Output/Debug/Obj/Camera/stm32l432xx_Vectors.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/Camera/stm32l432xx_Vectors.o: \
- /Users/neilchulani/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/Camera/STM32L4xx/Source/stm32l432xx_Vectors.s
